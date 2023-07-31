4f9fb17dd9bfdd16cabc81ce93ac867682112a8e
Alex Kozik <ajk333@cornell.edu>
2023-07-31 17:13:12 -0400

    Changed the theme

--- src/index/Index.tsx
+++ src/index/Index.tsx
@@ -1,9 +1,74 @@
 import "./index.css"
 
+
+function Welcome() {
+    return (
+        <div className = "welcome">
+            Welcome.
+        </div>
+    )
+}
+
+function IntroSection() {
+    return (
+        <div className = "intro-section">
+            <Welcome/>
+            <ul className = "desc-list">
+                <li className = "desc-list-item">
+                I am Alex, an aspiring software engineer from Ithaca, New York.
+                </li>
+                <li className = "desc-list-item">
+                I am currently a student at Cornell University, majoring in Computer Science and Mathematics.
+                </li>
+                <li className = "desc-list-item">
+                    I am interested in full-stack web development and programming language design.
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+function ProjectsHeader() {
+    return (
+        <div className = "projects-header">
+            Projects
+        </div>
+    )
+}
+
+function ProjectsSection() {
+    return (
+        <div className = "projects-section">
+            <ProjectsHeader />
+            <ul>
+                <ProjectItem name = "LambdaScript" />
+                <ProjectItem name = "HabitStack" />
+                <ProjectItem name = "CritterWorld" />
+                <ProjectItem name = "JSON Formatter" />
+            </ul>
+        </div>
+    )
+}
+
+function ProjectItem(props: {name: string}) {
+    return (
+        <>
+            <li className = "project-item">
+      
+                    {props.name}
+  
+            </li>
+        </>
+    )
+}
+    
+
+
 export default function Index() {
     return (
-        <div>
-            This is the index
+        <div className = "index-content">
+            <IntroSection />
+            <ProjectsSection />
         </div>
     )
 }
